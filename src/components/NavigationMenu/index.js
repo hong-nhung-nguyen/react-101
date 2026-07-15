e2f669e6436026303d0fddc9d0035d6b6177a6cb
Your Name <you@example.com>
2026-07-16 00:55:22 +1000

    Events

--- src/components/NavigationMenu/index.js
+++ src/components/NavigationMenu/index.js
@@ -1,7 +1,41 @@
 function NavigationMenu() {
+    const handleClick = (e) => {
+        console.log(e.target);
+    }
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+    }
+
+    const handleFocus = (e) => {
+        e.target.classList.add("input--active");
+        console.log(e.target);
+    }
+
+    const handleBlur = (e) => {
+        e.target.classList.remove("input--active");
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target[0].value);
+    }
+
     return (
         <>
-            <div className="box">Navigation Menu</div>
+            <div className="box">
+                <div>Navigation Menu</div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        className="input"
+                        type="text"
+                        onChange={handleChange}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                    />
+                <button onClick={handleClick} >Click</button>
+                </form>
+            </div>
         </>
     )
 };
